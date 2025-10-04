ac618d2f561a3d0666d3ebefb3a223482fa50f7d
ytsyarifsatu-prog <ytsyarifsatu@gmail.com>
2025-10-04 19:53:03 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, Response
+import cv2
+
+app = Flask(__name__)
+
+# Contoh stream video dari webcam / file
+def generate_frames():
+    camera = cv2.VideoCapture(0)  # bisa diganti path file video
+    while True:
+        success, frame = camera.read()
+        if not success:
+            break
+        else:
+            _, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/video')
+def video():
+    return Response(generate_frames(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
